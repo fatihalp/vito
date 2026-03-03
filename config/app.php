354e7ee9ff133c3cb505346c18ee357d74e61821
saeedvaziry <61919774+saeedvaziry@users.noreply.github.com>
2026-03-03 18:36:23 +0000

    Build version 3.20.2

--- config/app.php
+++ config/app.php
@@ -220,7 +220,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
-    'version' => '3.20.1',
+    'version' => '3.20.2',
 
     'demo' => env('APP_DEMO', false),
 
---
+++ public/build/assets/status-CBK6HAGA.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-DAiSR2mG.js";import{B as o}from"./badge-C9hJ0EgR.js";/* empty css            */import"./utils-BMbTliEY.js";import"./index-Bf-AJuo7.js";function u({server:t}){return r.jsx(o,{variant:t.status_color,children:t.status})}export{u as default};
